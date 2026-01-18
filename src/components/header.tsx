b59770c946bbca08da37419ce8462e228b65cf89
Prabakaran S R <prabakaransr198@gmail.com>
2026-01-18 15:26:24 +0530

    logo updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,14 +5,15 @@ import Image from "next/image";
 
 export const Header = () => {
     return (
-        <header className="fixed top-6 left-6 z-50">
+        <header className="fixed top-2 left-0 z-50">
             <Link href="/" className="block group">
-                <div className="relative w-16 h-16 md:w-20 md:h-20 transition-transform duration-300 group-hover:scale-105">
+                {/* Even larger and shifted up more */}
+                <div className="relative w-[400px] h-[140px] md:w-[900px] md:h-[340px] -mt-6 -ml-4 md:-mt-24 md:-ml-12 transition-transform duration-300 group-hover:scale-105">
                     <Image
                         src="/logo.png"
                         alt="Box Machi Box Logo"
                         fill
-                        className="object-contain drop-shadow-lg"
+                        className="object-contain object-left drop-shadow-lg"
                         priority
                     />
                 </div>
